4dc65117027662ba52398d6defe3520f56589a71
PYth0n6 <96932134+PYth0n6@users.noreply.github.com>
2021-12-31 23:25:58 -0500

    webhookspammer
    Epic spammer WOOHOOOO!!

---
+++ Kittyspammer.py
@@ -0,0 +1,21 @@
+#open source u should thank m - Bosscat/Pyth0n6
+from colorama import Fore
+from dhooks import Webhook
+
+print(Fore.CYAN + """
+╭╮╭━╮╭╮╱╭╮╱╱╱╱╭━━━╮
+┃┃┃╭╋╯╰┳╯╰╮╱╱╱┃╭━╮┃
+┃╰╯╯┣╮╭┻╮╭╋╮╱╭┫╰━━┳━━┳━━┳╮╭┳╮╭┳━━┳━╮
+┃╭╮┃┣┫┃╱┃┃┃┃╱┃┣━━╮┃╭╮┃╭╮┃╰╯┃╰╯┃┃━┫╭╯
+┃┃┃╰┫┃╰╮┃╰┫╰━╯┃╰━╯┃╰╯┃╭╮┃┃┃┃┃┃┃┃━┫┃
+╰╯╰━┻┻━╯╰━┻━╮╭┻━━━┫╭━┻╯╰┻┻┻┻┻┻┻━━┻╯
+╱╱╱╱╱╱╱╱╱╱╭━╯┃╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╰━━╯╱╱╱╱╰╯ anything you do i will not be charged for with u using this spammer you will agree if u use it.
+""")
+
+
+webhookurl = Webhook(input(Fore.BLUE + "Enter The webhook:"))
+
+while True:
+    webhookurl.send("KITTYSPAMMER ON TOP BITCH! ")
+    print(Fore.BLUE + "Message sent KITTYSPAMMER ON TOP")
